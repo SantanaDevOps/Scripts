c6a83753fb49b9432b16b02916beeb630c62c631
SantanaDevOps <csantanasec@protonmail.com>
2024-06-12 18:46:27 +0100

    ZIP file extraction script added

---
+++ PowerShell/zip-file-extraction/zip-file-extraction.ps1
@@ -0,0 +1,25 @@
+﻿# Ask the user for the directory path
+$rootPath = Read-Host "Please enter the path to the main directory"
+
+# Get the list of ZIP files within the current folder
+$zipFiles = Get-ChildItem -Path $rootPath -File -Filter "*.zip"
+
+# Iterate over each ZIP file
+foreach ($zipFile in $zipFiles) {
+    # Get the full path of the ZIP file
+    $zipFilePath = $zipFile.FullName
+
+    # Get the destination path to extract the ZIP file
+    $destinationPath = Join-Path -Path $rootPath -ChildPath $zipFile.BaseName
+
+    # Extract the ZIP file to the destination path
+    try {
+        Expand-Archive -Path $zipFilePath -DestinationPath $destinationPath -Force
+        Write-Host "Extracted ZIP file: $zipFilePath"
+    }
+    catch {
+        Write-Error "Error extracting ZIP file: $zipFilePath. Error details: $_"
+    }
+}
+
+Write-Host "ZIP file extraction completed."
